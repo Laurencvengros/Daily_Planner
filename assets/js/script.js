c782095e43b4a88d2cafd3284b12c93c4c2f6ad4
Lauren <l.cvengros@icloud.com>
2022-08-14 16:55:45 -0400

    assigned IDs to each row in HTML, created function is js to compare the current hour with the hours on the schedule, added classes in css to color code the schedule based on the time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,33 @@
 var digitalClock = function(){
 
 var todayDate = moment();
-$("#currentDay").text(todayDate.format('dddd MMM Do, YYYY, H:mm:ss A'));
+$("#currentDay").text(todayDate.format('dddd MMM Do, YYYY, H:mm:ss'));
 
 };
 
 digitalClock();
 setInterval(digitalClock, 1000);
 
+
+
+function trackHours(){
+        var currentTime = moment().hour();
+
+        $(".text").each(function(){
+            var calendarHour = parseInt($(this).attr("id"));
+
+            console.log(calendarHour);
+
+            if (calendarHour === currentTime){
+                $(this).addClass("current");
+
+            }else if( calendarHour < currentTime){
+                $(this).addClass("past");
+
+            }else{
+                $(this).addClass("future");
+            }
+
+
+        })
+    }
