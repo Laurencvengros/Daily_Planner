b309ca39c49d2216619f2420d13e586749c4ebac
Lauren <l.cvengros@icloud.com>
2022-08-14 09:24:12 -0400

    created function for clock to update without refreshing the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 //To Display Today's Date At The Top Of The Page.
-var currentTime = moment();
-$("#currentDay").text(currentTime.format('dddd MMM Do, YYYY, HHmm'));
-console.log(currentTime);
+var renderClock = function(){
 
+var todayDate = moment();
+$("#currentDay").text(todayDate.format('dddd MMM Do, YYYY, H:mm:ss A'));
+console.log(todayDate);
+};
 
-
-
+renderClock();
+setInterval(renderClock, 1000);
 
