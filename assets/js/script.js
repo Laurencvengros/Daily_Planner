363ce5e2f7675194a6e5bae716ef5cc284cf34b1
Lauren <l.cvengros@icloud.com>
2022-08-14 09:43:02 -0400

    added the first row of dates and gave them classes to style them in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 //To Display Today's Date At The Top Of The Page.
-var renderClock = function(){
+var digitalClock = function(){
 
 var todayDate = moment();
 $("#currentDay").text(todayDate.format('dddd MMM Do, YYYY, H:mm:ss A'));
 console.log(todayDate);
 };
 
-renderClock();
-setInterval(renderClock, 1000);
+digitalClock();
+setInterval(digitalClock, 1000);
 
