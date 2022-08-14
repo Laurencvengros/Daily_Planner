a2e5e6c1504c30acf846d2b82cbfd81223e60009
Lauren <l.cvengros@icloud.com>
2022-08-14 09:10:23 -0400

    linked CSS and JS to HTML and added date and time display

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//To Display Today's Date At The Top Of The Page.
+var currentTime = moment();
+$("#currentDay").text(currentTime.format('dddd MMM Do, YYYY, HHmm'));
+console.log(currentTime);
+
+
+
+
+
